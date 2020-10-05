07214fd48e35dde40d30f9a5b4586ed85a954d72
markamerkamp <markamerkamp@gmail.com>
2020-10-05 12:06:34 +0200

    Opdracht 9.2 onderdeel 1 en 2

--- src/controller/NamenBestandVerwerker.java
+++ src/controller/NamenBestandVerwerker.java
@@ -1,8 +1,43 @@
 package controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Author: Mark Amerkamp (markamerkamp@gmail.com)
  * Veel plezier ermee!
  */
 public class NamenBestandVerwerker {
+
+    public static void main(String[] args) {
+
+
+        String bestandsnaam = "resources/9.2 GesorteerdeNamenOrigineel.txt";
+        System.out.println(telNamenBestand(bestandsnaam));
+
+
+
+    }
+
+    public static int telNamenBestand(String bestandsnaam) {
+        File inputbestand = new File(bestandsnaam);
+        Scanner invoer = null;
+        int teller = 0;
+
+        try {
+            invoer = new Scanner(inputbestand);
+            while (invoer.hasNext()) {
+                invoer.next();
+                teller++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Bestand niet gevonden");
+        } finally {
+            if (invoer != null) {
+                invoer.close();
+            }
+        }
+        return teller;
+    }
 }
